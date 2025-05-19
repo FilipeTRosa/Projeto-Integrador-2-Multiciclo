91d8b80939be02293b98711b8e8bdfb9e3868948
Filipe Teixeira da Rosa <114033595+FilipeTRosa@users.noreply.github.com>
2025-05-18 23:48:07 -0300

    Delete MiniMips8BitsMonociclo directory

--- MiniMips8BitsMonociclo/Readme.txt
+++
@@ -1,1 +0,0 @@
-Condição de parada do programa (add $0, $0, $0) // Última instrução deve ser uma instrução no formato 0000000000000000
--- MiniMips8BitsMonociclo/step.h
+++
@@ -1,3 +0,0 @@
-
-
-void step(int *parada,int *pc, struct memoria_instrucao *memInst, BRegs *bancoReg, CTRL *controle, descPilha *pilha, struct estatistica *stat, int *estadoControle, int *regSaidaUla, RegMDR* regMDR, int *regA, int *regB, RegINST* IR);
